d44a9d4d33f3ee54efe4639e4eddf1d9c95247a3
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-04-22 19:14:11 -0300

    repaginação section projetos

--- videoProjetos.js
+++ videoProjetos.js
@@ -5,13 +5,13 @@ let imagemPatinhas = document.querySelector("#sitePatinhas");
 // Adiciona um ouvinte de evento para quando o mouse passar por cima da imagem
 boxImagem.addEventListener("mouseover", function () {
   // Altera o atributo src da imagem para a nova imagem
-  imagemPatinhas.src = "videoPatinhas.gif";
+  imagemPatinhas.src = "assets/imagensProjetos/sitePatinhas.gif";
 });
 
 // Adiciona um ouvinte de evento para quando o mouse sair da imagem (para voltar à imagem original, se necessário)
 boxImagem.addEventListener("mouseout", function () {
   // Volta o atributo src da imagem para a imagem original
-  imagemPatinhas.src = "fotoPatinhas.png";
+  imagemPatinhas.src = "assets/imagensProjetos/sitePatinhas.png";
 });
 
 
@@ -22,13 +22,13 @@ let happyBowl = document.querySelector("#siteHappyBowl");
 // Adiciona um ouvinte de evento para quando o mouse passar por cima da imagem
 boxImagemTwo.addEventListener("mouseover", function () {
   // Altera o atributo src da imagem para a nova imagem
-  happyBowl.src = "gif2.gif";
+  happyBowl.src = "assets/imagensProjetos/siteHappyBowl.gif";
 });
 
 // Adiciona um ouvinte de evento para quando o mouse sair da imagem (para voltar à imagem original, se necessário)
 boxImagemTwo.addEventListener("mouseout", function () {
   // Volta o atributo src da imagem para a imagem original
-  happyBowl.src = "siteHappyBowl.png";
+  happyBowl.src = "assets/imagensProjetos/siteHappyBowl.png";
 });
 
 
@@ -39,13 +39,13 @@ let easyBank = document.querySelector("#siteEasy");
 // Adiciona um ouvinte de evento para quando o mouse passar por cima da imagem
 boxImagemThree.addEventListener("mouseover", function () {
   // Altera o atributo src da imagem para a nova imagem
-  easyBank.src = "easybank.gif";
+  easyBank.src = "assets/imagensProjetos/siteEaysBank.gif";
 });
 
 // Adiciona um ouvinte de evento para quando o mouse sair da imagem (para voltar à imagem original, se necessário)
 boxImagemThree.addEventListener("mouseout", function () {
   // Volta o atributo src da imagem para a imagem original
-  easyBank.src = "easy.png";
+  easyBank.src = "assets/imagensProjetos/siteEaysBank.png";
 });
 
 
