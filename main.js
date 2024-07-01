0eb371db99e01049cca29a7f8bcb864bfb14d359
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-06-30 21:22:03 -0300

    att area projetos

--- main.js
+++ main.js
@@ -111,3 +111,16 @@ particlesJS({
   },
   retina_detect: true,
 });
+
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
